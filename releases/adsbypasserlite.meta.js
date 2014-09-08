6c272b1fad39394ab6749b17afc7774ead1cd326
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-09-08 15:49:19 +0900

    Initial commit.

---
+++ releases/adsbypasserlite.meta.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name           AdsBypasserLite
+// @namespace      AdsBypasser
+// @description    Bypass Ads
+// @copyright      2012+, Wei-Cheng Pan (legnaleurc)
+// @version        5.0.0
+// @license        BSD
+// @updateURL      https://adsbypasser.github.io/releases/adsbypasserlite.meta.js
+// @downloadURL    https://adsbypasser.github.io/releases/adsbypasserlite.user.js
+// @grant          unsafeWindow
+// @grant          GM_xmlhttpRequest
+// @grant          GM_addStyle
+// @grant          GM_getResourceText
+// @grant          GM_getResourceURL
+// @grant          GM_getValue
+// @grant          GM_openInTab
+// @grant          GM_registerMenuCommand
+// @grant          GM_setValue
+// @run-at         document-start
+// @resource       alignCenter https://raw.githubusercontent.com/adsbypasser/adsbypasser/v5.0.0/css/align_center.css
+// @resource       scaleImage https://raw.githubusercontent.com/adsbypasser/adsbypasser/v5.0.0/css/scale_image.css
+// @resource       bgImage https://raw.githubusercontent.com/adsbypasser/adsbypasser/v5.0.0/img/imagedoc-darknoise.png
+// @include        http://*
+// @include        https://*
+// ==/UserScript==
