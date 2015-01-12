3767a696830a87633b65a21ddf65e389d51c6dd5
Wei-Cheng Pan <legnaleurc@gmail.com>
2015-01-12 12:31:06 +0800

    Update content.

--- releases/adsbypasserlite.user.js
+++ releases/adsbypasserlite.user.js
@@ -3,26 +3,22 @@
 // @namespace      AdsBypasser
 // @description    Bypass Ads
 // @copyright      2012+, Wei-Cheng Pan (legnaleurc)
-// @version        5.11.0
+// @version        5.11.1
 // @license        BSD
 // @homepageURL    https://adsbypasser.github.io/
 // @supportURL     https://github.com/adsbypasser/adsbypasser/issues
 // @updateURL      https://adsbypasser.github.io/releases/adsbypasserlite.meta.js
 // @downloadURL    https://adsbypasser.github.io/releases/adsbypasserlite.user.js
-// @icon           https://raw.githubusercontent.com/adsbypasser/adsbypasser/v5.11.0/img/logo.png
+// @icon           https://raw.githubusercontent.com/adsbypasser/adsbypasser/v5.11.1/img/logo.png
 // @grant          unsafeWindow
 // @grant          GM_xmlhttpRequest
-// @grant          GM_addStyle
-// @grant          GM_getResourceText
-// @grant          GM_getResourceURL
+
 // @grant          GM_getValue
 // @grant          GM_openInTab
 // @grant          GM_registerMenuCommand
 // @grant          GM_setValue
 // @run-at         document-start
-// @resource       alignCenter https://raw.githubusercontent.com/adsbypasser/adsbypasser/v5.11.0/css/align_center.css
-// @resource       scaleImage https://raw.githubusercontent.com/adsbypasser/adsbypasser/v5.11.0/css/scale_image.css
-// @resource       bgImage https://raw.githubusercontent.com/adsbypasser/adsbypasser/v5.11.0/img/imagedoc-darknoise.png
+
 // @include        http://*
 // @include        https://*
 // ==/UserScript==
@@ -374,14 +370,16 @@
         data: data,
         headers: headers,
         onload: function (response) {
-          if (this.status !== 200) {
-            reject(this.responseText);
+          response = (typeof this.responseText !== 'undefined') ? this : response;
+          if (response.status !== 200) {
+            reject(response.responseText);
           } else {
-            resolve(this.responseText);
+            resolve(response.responseText);
           }
         },
         onerror: function (response) {
-          reject(this.responseText);
+          response = (typeof this.responseText !== 'undefined') ? this : response;
+          reject(response.responseText);
         },
       });
     });
